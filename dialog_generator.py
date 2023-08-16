d6ec76b28fdd4bd385f90eeb91c3fbf215c3c325
eugenos-programos <elena.samchuk.73@mail.ru>
2023-08-16 12:33:03 +0300

    add dialog generator and synthesizer

--- dialog_generator.py
+++ dialog_generator.py
@@ -1,10 +1,29 @@
 from gradio_client import Client
+from argparse import ArgumentParser
+import pyttsx3
 
+
+arg_parser = ArgumentParser(
+    prog="Dialog Generator"
+)
+
+arg_parser.add_argument("-l", "--level", type=str, required=True, help="English proficiency level")
+arg_parser.add_argument("-n", "--n-person", type=int, default=2, help="N person interacting")
+arg_parser.add_argument("--topik", type=str, help="dialog topic")
+
+args = arg_parser.parse_args()
+topic = f"aboout {args.topik}" if args.topik is not None else ""
+prompt = f"generate a dialog between {args.n_person} person in {args.level} English proficiency level" + topic
 
 client = Client("https://ysharma-explore-llamav2-with-tgi.hf.space/")
-for _ in range(5):
-    result = client.predict(
-                    "Generate a dialog between two persons in A1 English level",	# str in 'Message' Textbox component
-                    api_name="/chat"
-    )
-    print(result, end='\n__________________________________\n')
+result = client.predict(
+                    prompt,
+                    api_name="/chat")
+print(result, end='\n__________________________________\n')
+
+
+from gtts import gTTS
+
+myobj = gTTS(text=result, lang='en', slow=False, tld='ie')
+myobj.save("test.mp3")
+

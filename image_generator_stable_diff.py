654328ae56211e4cf2256c3a5554ff6b10902dcc
eugenos-programos <elena.samchuk.73@mail.ru>
2023-08-17 14:40:02 +0300

    add word description

--- image_generator_stable_diff.py
+++ image_generator_stable_diff.py
@@ -17,33 +17,41 @@ parser = argparse.ArgumentParser(
     prog="text2image"
 )
 
-parser.add_argument("-f", "--file", help="JSON file path", required=True)
+parser.add_argument("-f", "--file", help="JSON file path", required=True, type=str)
+parser.add_argument("-n", help="N images", required=False, default=1, type=int)
+parser.add_argument("--no-save", help="save image on GDrive or not", default=True, required=False, action="store_true")
 args = parser.parse_args()
 file_path = args.file
+n_images = args.n
+no_save = args.no_save
 
 with open(file_path) as file:
     words = json.load(file)
 
 
-
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-
-
-gauth = GoogleAuth()        
-gauth.LocalWebserverAuth()   
-drive = GoogleDrive(gauth)  
-
-
 for prof_level in words.keys():
-    for word_dict in words[prof_level]:
-        word = word_dict["word"]
-        image = Image.open(get_predictions(word))
+    for theme in words[prof_level]:
+        for word_dict in words[prof_level][theme]:
+            word = word_dict["word"]
+            definition = word_dict["definitions"][0]
+            for idx in range(n_images):
+                image = Image.open(get_predictions(word + ' - ' + definition))
 
-        img_path = f"images/{word}.png"
-        image.save(img_path)
-        gd_file = drive.CreateFile({'title': f"{word}.png"})
-        gd_file.SetContentFile(img_path)
-        gd_file.Upload()
+                img_path = f"images/{word}-{idx}.png"
+                image.save(img_path)
+                if not no_save:
 
-        gd_file = None
+
+                    from pydrive.auth import GoogleAuth
+                    from pydrive.drive import GoogleDrive
+
+
+                    gauth = GoogleAuth()        
+                    gauth.LocalWebserverAuth()   
+                    drive = GoogleDrive(gauth)  
+
+                    gd_file = drive.CreateFile({'title': f"{word}.png"})
+                    gd_file.SetContentFile(img_path)
+                    gd_file.Upload()
+
+                    gd_file = None
